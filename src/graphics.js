20e12809c91a40a678b676b7bdfbb62da4e64887
Q <marq46.steam@googlemail.com>
2015-09-04 18:19:40 +0100

    w0rking simple draw

--- src/graphics.js
+++ src/graphics.js
@@ -3,9 +3,10 @@ var c = null;
 var gl = null;
 var prog = null;
 
-// shader vars
-var vs = null;
-var fs = null;
+var vert_buff = null;
+
+var vec_pos = null;
+var obj_pos = null;
 
 // init function called on page load
 function gl_init() {
@@ -21,11 +22,31 @@ function gl_init() {
 
 	if (gl == null) { alert("panic at gl context"); }
 
-	vs = grep_shader("shader-vs", gl);
-	fs = grep_shader("shader-fs", gl);
+	var vs = grep_shader("shader-vs", gl);
+	var fs = grep_shader("shader-fs", gl);
 	prog = create_prog(vs, fs, gl);
 
+	// shader atrabutes
+	vec_pos = gl.getAttribLocation(prog, "vec_pos");
+	gl.enableVertexAttribArray(vec_pos);
+
+	//shader uniforms
+	obj_pos = gl.getUniformLocation(prog, "obj_pos");
+
 	gl.useProgram(prog);
+
+	var quad_vertex = [
+		0.5, 0.5, 0,
+		-0.5, 0.5, 0,
+		0.5, -0.5, 0,
+		-0.5, -0.5, 0
+	];
+	
+	vert_buff = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vert_buff);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(quad_vertex), gl.STATIC_DRAW);
+	gl.vertexAttribPointer(vec_pos, 3, gl.FLOAT, false, 0, 0);
+	gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
 	render_loop();
 }
@@ -37,4 +58,10 @@ function render_loop() {
 	gl.clearColor(0.05, 0.05, 0.05, 1.0);
 	gl.enable(gl.DEPTH_TEST);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	gl.uniform3fv(obj_pos, new Float32Array([0, 0, 0]));
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, vert_buff);
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+	gl.bindBuffer(gl.ARRAY_BUFFER, null);
 }
