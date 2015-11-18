ebdd0a7690954963d70484fdecac821fefb44303
EagleAngelo <EagleAngelo@users.noreply.github.com>
2015-11-17 22:43:03 -0600

    Update py3bot.py

--- py3bot.py
+++ py3bot.py
@@ -7,27 +7,30 @@ import string
 HOST = "irc.freenode.net"
 PORT = 6667
 
-NICK = ""
-IDENT = ""
-REALNAME = ""
-MASTER = ""
+NICK = "Oka_Nieba"
+IDENT = "guttenMorgen"
+REALNAME = "Oka_Nieba"
+MASTER = "Cheshire"
+#CHANNEL = "#PBSIdeaChannel"
+CHANNEL = "#botTesting"
 
 readbuffer = ""
 
 s=socket.socket( )
 s.connect((HOST, PORT))
 
-s.send(bytes("NICK %s\r\n" % NICK, "UTF-8"))
-s.send(bytes("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME), "UTF-8"))
-s.send(bytes("JOIN #botTesting\r\n", "UTF-8"));
+s.send(bytes("NICK "+ NICK + "\r\n", "UTF-8"));
+s.send(bytes("USER "+ IDENT + " " + HOST + " " + REALNAME + ":This is a bot thingy \r\n", "UTF-8"));
+s.send(bytes("JOIN "+ CHANNEL + "\r\n", "UTF-8"));
+
 s.send(bytes("PRIVMSG %s :Hello, testing 1 2 3\r\n" % MASTER, "UTF-8"))
 
 while 1:
-    readbuffer = readbuffer+s.recv(1024).decode("UTF-8") #readbuffer = readbuffer+s.recv(512).decode("UTF-8")
-    temp = str.split(readbuffer, "\n") #remove
-    readbuffer=temp.pop( ) #remove
+    readbuffer = readbuffer+s.recv(512).decode("UTF-8")
+    temp = str.split(readbuffer, "\n")
+    readbuffer=temp.pop( )
 
-    for line in temp: #for line in readbuffer:
+    for line in temp:
         line = str.rstrip(line)
         line = str.split(line)
 
@@ -35,7 +38,7 @@ while 1:
             s.send(bytes("PONG %s\r\n" % line[1], "UTF-8"))
 
         if(line[1] == "PRIVMSG"):
-            if(line[3] == ":!echo"):
+            if(line[3] == ":!msgMe"):
                 sender = ""
                 for char in line[0]:
                     if(char == "!"):
@@ -44,13 +47,11 @@ while 1:
                         sender += char 
                 size = len(line)
                 i = 3
-                message = ""
-                while(i < size): 
-                    message += line[i] + " "
-                    i = i + 1
-                message.lstrip(":")
-                s.send(bytes("PRIVMSG %s %s \r\n" % (sender, message), "UTF-8"))
+                message = "OH HAI!" 
+                s.send(bytes("PRIVMSG "+ sender + " :" + message + "\r\n", "UTF-8"))
             elif(line [3] == ":!quit"):
                 s.send(bytes("QUIT %s\r\n", "UTF-8"))
+            elif(line [3] == ":!ping"):
+                s.send(bytes("PRIVMSG "+ CHANNEL + " :PONG!!! (╯°□°)╯︵ ┻━┻\r\n", "UTF-8"))
         for index, i in enumerate(line):
             print(line[index], index)
