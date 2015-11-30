4d8834a92154bc04e27f6ad8f8d8a31a4f6a4388
EagleAngelo <EagleAngelo@users.noreply.github.com>
2015-11-30 17:06:26 -0600

    Update ItsyBitsy.py

--- ItsyBitsy.py
+++ ItsyBitsy.py
@@ -4,74 +4,102 @@ import os
 import sys
 import re
 
+clear = lambda: os.system('cls')
+
 wb = win32com.client.Dispatch("InternetExplorer.Application")
 wb.Visible = 0
-wb.Navigate("www.google.com")
 
-while (wb.busy): #wb.ReadyState != 4 or wb.Document.Readystate != "Complete"):
+while (wb.busy): 
  time.sleep(5)
 
-#wb.Document.GetElementById("USER").Value == ""
-#wb.Document.GetElementById("password").Value == ""
+site = ""
 
-#while (wb.busy):
-# time.sleep(10)
+clear()
 
-#wb.document.all.IMAGE1.click()
+print("I think Berra is a pretty cool guy if he reads this message :D")
 
 while True:
- action = input("\nPress q + Enter to quit. c + Enter to continue. \n")
+ print("Welcome!")
+ if not site:
+  print("No website is currently selected")
+ else:
+  print(site, "is the currently selected website")
+  
+ action = input("\nq = quit, w = change website, c = continue\n")
  
  if action == 'q':
   wb.quit()
+  clear()
   sys.exit("\nExiting Script\n")
-  
+ 
+ elif action == 'w':
+  site = input("\nPlease enter the website's address or IP\n")
+  clear()
+   
  elif action == 'c':
-  text = wb.document.documentElement.innerText
-  html = wb.document.documentElement.innerHTML
-
-  term = input("\nData retrieved, please input search term: \n")
-  
-  seachingText = re.finditer(term,text,re.I)
-  seachingHTML = re.finditer(term,html,re.I)
-  
-  Textfound = re.search(term,text,re.I)
-  HTMLfound = re.search(term,html,re.I)
-
-  if Textfound:
-   print ("\nDocument.text: \n")
-   for s in seachingText:
-    print (s.start(),s.end(),s.group())
+  if not site:
+   print("\nThere is no website selected\n")
+   cont = input("\nPress any key to continue\n")
+   clear()
   else:
-   print ("\nNo matches in Document.text\n")
   
-  if HTMLfound:
-   print ("\nDocument.HTML: \n")
-   for s in seachingHTML:
-    print (s.start(),s.end(),s.group())
-  else:
-   print ("\nNo matches in Document.HTML\n")
-  
-  extend = input("\nExtend lines? y/n \n")
-  
-  if extend == 'y':
-   seachingText = re.finditer('(.{25})'+term+'(.{25})',text,re.I)
-   seachingHTML = re.finditer('(.{25})'+term+'(.{25})',html,re.I)
+   print("\nLooking up website, this may take a moment")
    
-   if Textfound:
-    print ("\nDocument.text: \n")
-    for s in seachingText:
-     print (s.start(),s.end(),s.group())
-   else:
-    print ("\nNo matches in Document.text\n")
+   wb.Navigate(site)
+   
+   while (wb.busy):   
+    time.sleep(5)
+   
+   text = wb.document.documentElement.innerText
+   html = wb.document.documentElement.innerHTML
+   
+   if len(text) > 0 or len(html) > 0:
+    term = input("\nData retrieved, please input search term: \n")
+  
+    seachingText = re.finditer(term,text,re.I)
+    seachingHTML = re.finditer(term,html,re.I)
+  
+    Textfound = re.search(term,text,re.I)
+    HTMLfound = re.search(term,html,re.I)
 
-   if HTMLfound:
-    print ("\nDocument.HTML: \n")
-    for s in seachingHTML:
-     print (s.start(),s.end(),s.group())
-   else:
-    print ("\nNo matches in Document.HTML\n")
+    if Textfound:
+     print ("\nDocument.text: \n")
+     for s in seachingText:
+      print (s.start(),s.end(),s.group())
+    else:
+     print ("\nNo matches in Document.text\n")
+  
+    if HTMLfound:
+     print ("\nDocument.HTML: \n")
+     for s in seachingHTML:
+      print (s.start(),s.end(),s.group())
+    else:
+     print ("\nNo matches in Document.HTML\n")
    
+    extend = input("\nExtend lines? y/n \n")
+   
+    if extend == 'y':
+     seachingText = re.finditer('(.{25})'+term+'(.{25})',text,re.I)
+     seachingHTML = re.finditer('(.{25})'+term+'(.{25})',html,re.I)
+     
+     if Textfound:
+      print ("\nDocument.text: \n")
+      for s in seachingText:
+       print (s.start(),s.end(),s.group())
+     else:
+      print ("\nNo matches in Document.text\n")       
+
+     if HTMLfound:
+      print ("\nDocument.HTML: \n")
+      for s in seachingHTML:
+       print (s.start(),s.end(),s.group())
+     else:
+      print ("\nNo matches in Document.HTML\n")
+   else:
+    print("No data could be found within the specified website")
+   
+   cont = input("\nPress any key to continue\n")
+   clear()
 
 #path1 = "C:\dat.txt"
 #path1 =  os.path.normpath(path1)
@@ -82,3 +110,11 @@ while True:
 
 #print(text)
 #print(html)
+
+#---
+#wb.ReadyState != 4 or wb.Document.Readystate != "Complete"):
+#wb.Document.GetElementById("USER").Value == ""
+#wb.Document.GetElementById("password").Value == ""
+#while (wb.busy):
+# time.sleep(10)
+#wb.document.all.IMAGE1.click()
